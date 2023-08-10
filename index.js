db397041866a6c7e77711cf75473bdd8715d6a32
Dmitrii Tsimintiia <idimit@MacBook-Pro-Dmitrii.local>
2023-08-10 22:24:21 +0400

    - async/await

--- index.js
+++ index.js
@@ -1,83 +1,78 @@
 const data_val = {
-    valute: [],
-}
+  valute: [],
+};
 
-const valute_block = document.querySelector('.valute_block');
-const valute = document.querySelector('.valute');
+const valute_block = document.querySelector(".valute_block");
+const valute = document.querySelector(".valute");
 
+function get_valute() {
+  fetch("https://www.cbr-xml-daily.ru/daily_json.js").then(function (response) {
+    response.json().then(function (json) {
+      let arr = json.Valute;
+      data_val.valute = [];
+      valute.innerHTML = "";
+      for (key in arr) {
+        let ob = {
+          CharCode: arr[key].CharCode,
+          Name: arr[key].Name,
+          Nominal: arr[key].Nominal,
+          Value: arr[key].Value,
+        };
+        data_val.valute.push(ob);
+      }
 
-async function get_valute() {
-    await fetch('https://www.cbr-xml-daily.ru/daily_json.js').then(function (response) {
-        response.json().then(function (json) {
+      //// Если раскоментировать то можно использовать пагинацию
 
-            let arr = json.Valute;
-            data_val.valute = [];
-            valute.innerHTML = '';
-            for (key in arr) {
-                let ob = {
-                    CharCode: arr[key].CharCode,
-                    Name: arr[key].Name,
-                    Nominal: arr[key].Nominal,
-                    Value: arr[key].Value
-                };
-                data_val.valute.push(ob)
-            }
+      // const pagin = document.querySelector('.pagin');
 
-            //// Если раскоментировать то можно использовать пагинацию
+      // let pagin_count;
 
-            // const pagin = document.querySelector('.pagin');
+      // let lim;
 
-            // let pagin_count;
+      // function class_add(e, className) {
+      //     e.classList.add(className)
+      // }
 
+      // function class_reemuve(e, className) {
+      //     e.classList = className;
+      // }
 
-            // let lim;
+      // pagin_count = Math.ceil(data_val.valute.length / 5);
+      // for (let i = 1; i <= pagin_count; i++) {
+      //     pagin.innerHTML += `<button class="pagin_btn " id=${i === 1 ? i : (i - 1) * 5}>${i}</button>`;
+      //     const pagin_btn = document.querySelectorAll('.pagin_btn');
+      //     class_add(pagin_btn[0], 'btn_active');
+      //     pagin_btn.forEach(item => item.addEventListener('click', (e) => {
 
-            // function class_add(e, className) {
-            //     e.classList.add(className)
-            // }
+      //         pagin_btn.forEach(item => {
+      //             class_reemuve(item, 'pagin_btn')
+      //         })
+      //         lim = +e.target.id - 1;
+      //         valut_render(lim)
+      //         class_add(e.target, 'btn_active')
+      //         // console.log(e);
+      //     }))
+      // }
 
-            // function class_reemuve(e, className) {
-            //     e.classList = className;
-            // }
-
-            // pagin_count = Math.ceil(data_val.valute.length / 5);
-            // for (let i = 1; i <= pagin_count; i++) {
-            //     pagin.innerHTML += `<button class="pagin_btn " id=${i === 1 ? i : (i - 1) * 5}>${i}</button>`;
-            //     const pagin_btn = document.querySelectorAll('.pagin_btn');
-            //     class_add(pagin_btn[0], 'btn_active');
-            //     pagin_btn.forEach(item => item.addEventListener('click', (e) => {
-
-            //         pagin_btn.forEach(item => {
-            //             class_reemuve(item, 'pagin_btn')
-            //         })
-            //         lim = +e.target.id - 1;
-            //         valut_render(lim)
-            //         class_add(e.target, 'btn_active')
-            //         // console.log(e);
-            //     }))
-            // }
-
-            function valut_render(lim = 0) {
-                valute.innerHTML = '';
-                data_val.valute.map((item, i) => {
-                    let valute_dat = '';
-                    valute_dat = `<div class="valute_text ">Цена ${item.Nominal} ${item.Name} (${item.CharCode}) = ${+item.Value.toFixed(2)} руб</div>`;
-                    valute.innerHTML += valute_dat;
-                    // if (i >= lim && i < lim + 5) {
-                    //     valute_dat = `<div class="valute_text ">Цена ${item.Nominal} ${item.Name} (${item.CharCode}) = ${+item.Value.toFixed(2)} руб</div>`;
-                    //     valute.innerHTML += valute_dat;
-                    // }
-                })
-            }
-            valut_render()
-            // console.log(arr)
-            // console.log(data_val)
-
-
+      function valut_render(lim = 0) {
+        valute.innerHTML = "";
+        data_val.valute.map((item, i) => {
+          let valute_dat = "";
+          valute_dat = `<div class="valute_text ">Цена ${item.Nominal} ${
+            item.Name
+          } (${item.CharCode}) = ${+item.Value.toFixed(2)} руб</div>`;
+          valute.innerHTML += valute_dat;
+          // if (i >= lim && i < lim + 5) {
+          //     valute_dat = `<div class="valute_text ">Цена ${item.Nominal} ${item.Name} (${item.CharCode}) = ${+item.Value.toFixed(2)} руб</div>`;
+          //     valute.innerHTML += valute_dat;
+          // }
         });
+      }
+      valut_render();
+      // console.log(arr)
+      // console.log(data_val)
     });
+  });
 }
-
-
 
 get_valute();
